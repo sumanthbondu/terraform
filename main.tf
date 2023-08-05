4b1de1f29403d93776ac676604428a7bdb35c760
sumanthbondu <40199161+sumanthbondu@users.noreply.github.com>
2023-08-05 17:38:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2"{
     ami = "ami-024fc608af8f886bc"
     instance_type = "t2.micro"
-    security_groups = [aws.aws_security_group.webtraffic.name]
+    #security_groups = [aws.aws_security_group.webtraffic.name]
 }
 
 resource "aws_eip" "elasticip"{
@@ -14,24 +14,6 @@ resource "aws_eip" "elasticip"{
 
 output "EIP"{
     value = aws_eip.elasticip.public_ip
-}
-
-resource "aws_security_group" "webtraffic" {
-    name = "Allow HTTPS"
-
-    ingress{
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress{
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 }
 
 resource "aws_vpc" "main" {
