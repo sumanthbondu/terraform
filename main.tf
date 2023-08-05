f824229c81939f51fe8427c8c148ab917a000dd9
sumanthbondu <40199161+sumanthbondu@users.noreply.github.com>
2023-08-05 17:45:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,22 @@ variable "private_subnet_cidrs" {
  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
