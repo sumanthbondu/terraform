d3b55dff78409a821a8e7e418d8442a1d5d3f261
sumanthbondu <40199161+sumanthbondu@users.noreply.github.com>
2023-08-05 18:15:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ variable "private_subnet_cidrs" {
 variable "azs" {
  type        = list(string)
  description = "Availability Zones"
- default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 resource "aws_subnet" "public_subnets" {
